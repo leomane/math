e1edb186740c6e1aa6a26adc1640ec28dce05e96
Anthony Leo <ay6254@wayne.edu>
2017-03-02 15:24:40 -0500

    Added comment to orthocenter

--- orthocenter.py
+++ orthocenter.py
@@ -7,6 +7,7 @@ import sys
 # The orthocenter of a triangle is where
 # the altitude of all three sides meet
 
+
 def get_points():
 	points = []
 	points.append(get_point('first'))
@@ -26,6 +27,17 @@ def slope(a,b):
 
 def inverse(a):
 	return float(1 / a * -1)
+
+# This function, used to calculate the orthocenter (zx, zy) of
+# the triangle, begins with writting the altitude running through 
+# any given point of the triangle in point-slope form as 
+# zy - y1 = m(zx - x1),which can be rewritten in y intercept form as
+# y = m(zx - x1) + y1. Here, x1 and y1 represent the x and y coordinates 
+# of any of point on the triangle. First, zx is solved for by setting 
+# the y intercept equations for the first two points equal to each other.
+# This results in the equation m1(zx - x1) = m2(zx - x2), which can be 
+# rewritten as zx = (m1x1 - m2x2 + y2 - y1) / (m1 - m2). Once zx is 
+# calculated, it's pluged back into the y-int equation to solve for zy
 
 def orthocenter(points):
 	a = points[0]
